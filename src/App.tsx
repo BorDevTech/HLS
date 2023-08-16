f0aa1c6a8c258a239093a4f908143c2070084d2c
Devell Robinson <devell.robinson@icloud.com>
2023-08-16 18:20:50 -0400

    Update App.tsx
    Resolve error

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <li><a href={"/"}>HLS</a></li>
             <li><input placeholder={"search"}/></li>
             <li><button>Favorites</button></li>
-            <li><button><a href={"/login"}Login</a></button></li>
+            <li><button><a href={"/login"}>Login</a></button></li>
             <li><button>Profile</button></li>
             </ul>
     </nav>
