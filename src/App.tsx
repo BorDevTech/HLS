f12744390e124d57465cbcb295f45a6b4b7f72ad
Devell Robinson <devell.robinson@icloud.com>
2023-08-16 18:17:52 -0400

    Update App.tsx
    Added favorites, profile, and login button

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
           <ul>
             <li><a href={"/"}>HLS</a></li>
             <li><input placeholder={"search"}/></li>
+            <li><button>Favorites</button></li>
+            <li><button><a href={"/login"}Login</a></button></li>
+            <li><button>Profile</button></li>
             </ul>
     </nav>
       <br />
