60a3b053e8bfcbe7dcae150f0320e9443f1122ac
BorDevTech <bordevtech@icloud.com>
2023-08-17 04:26:40 -0400

    added colorMode and applied to site

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/HLS/">
-      <App />
-    </Router>
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Router basename="/HLS/">
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>
 );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
+import { useColorMode } from "@chakra-ui/react";
 
 const Home = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
+      <button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "dark" : "light"}
+      </button>
       <br />
       Welcome
       <br />
