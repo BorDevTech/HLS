e43c30697375ef9e85f4b7c09536ca800cdb5168
BorDevTech <bordevtech@icloud.com>
2023-08-21 03:22:31 -0400

    SearchBar section MVP completed

--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -1,7 +1,25 @@
+import * as CUR from "@chakra-ui/react";
+import * as CUI from "@chakra-ui/icons";
+import { useColorMode } from "@chakra-ui/react";
+
 const SearchBar = () => {
+  const { colorMode } = useColorMode();
   return (
     <>
-      <input placeholder={"search"} />
+      <CUR.InputGroup>
+        <CUR.Input placeholder={"search"} />
+        <CUR.InputRightElement>
+          <CUR.IconButton
+            colorScheme="blackAlpha.50"
+            aria-label="Search database"
+            icon={
+              <CUI.SearchIcon
+                color={colorMode === "light" ? "#000000" : "#FFFFFF"}
+              />
+            }
+          />
+        </CUR.InputRightElement>
+      </CUR.InputGroup>
     </>
   );
 };
