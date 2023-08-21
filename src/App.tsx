6c78ce30e6e21be3ac53c5285b3756fa84a42dcc
BorDevTech <bordevtech@icloud.com>
2023-08-20 23:45:57 -0400

    working on mobile display

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,73 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { Home, Login, Profile, Register } from "./routes";
+import * as CUR from "@chakra-ui/react";
+import * as CUI from "@chakra-ui/icons";
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">
-              <h1>HLS</h1>
-            </Link>
-          </li>
-          <li>
-            <input placeholder={"search"} />
-          </li>
-          <li>
-            <Link to="/profile/favorites">
-              <button>Favorites</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/guides/faq">
-              <button>Help</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/guides/faq">
-              <button>Add Listing</button>
-            </Link>
-          </li>
-
-          <li>
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile">
-              <button>Profile</button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/favorites" element={<Home />} />
-      </Routes>
+      <CUR.Grid
+        templateAreas={{
+          base: `"Navbar" "Main" "Footer"`,
+          md: `"Navbar" "Main" "Footer"`,
+          lg: `"Navbar" "Main" "Footer"`,
+        }}
+        minH={window.innerHeight}
+        minW={window.innerWidth}
+      >
+        <CUR.GridItem area={"Navbar"}>
+          <nav>
+            <CUR.Grid
+              templateRows={"repeat(5,1fr)"}
+              templateAreas={{
+                base: `"Brand Buttons"`,
+                md: `"Brand Navigation"`,
+                lg: `"Brand Search Navigation Buttons"`,
+              }}
+            >
+              <CUR.GridItem area={"Brand"}>
+                <Link to="/">HLS</Link>
+              </CUR.GridItem>
+              <CUR.Show above={"lg"}>
+                <CUR.GridItem area={"Search"}>
+                  <input placeholder={"search"} />
+                </CUR.GridItem>
+                <CUR.Show above={"md"}>
+                  <CUR.GridItem area={"Navigation"}>
+                    <Link to="/guides/faq">Help</Link>
+                    <Link to="/guides/faq/addListing">Add Listing</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/profile">Profile</Link>
+                  </CUR.GridItem>
+                </CUR.Show>
+              </CUR.Show>
+              <CUR.GridItem area={"Buttons"}>
+                <Link to="/profile/favorites">
+                  <CUR.VStack>
+                    <CUI.StarIcon />
+                    <CUR.Text>Favorites</CUR.Text>
+                  </CUR.VStack>
+                </Link>
+              </CUR.GridItem>
+            </CUR.Grid>
+          </nav>
+        </CUR.GridItem>
+        <CUR.GridItem area={"Main"}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/favorites" element={<Home />} />
+            <Route path="/guides/faq" element={<Home />} />
+            <Route path="/guides/faq/addListing" element={<Home />} />
+          </Routes>
+        </CUR.GridItem>
+        <CUR.GridItem area={"Footer"}>
+          <p>footer</p>
+        </CUR.GridItem>
+      </CUR.Grid>
     </>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,8 +10,6 @@ const Home = () => {
       <br />
       Welcome
       <br />
-      Footer
-      <br />
     </>
   );
 };
